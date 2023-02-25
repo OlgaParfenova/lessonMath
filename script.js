358c147633887a5507901cec8b10110d551e57b2
Olga Parfenova <work.parfenova@gmail.com>
2023-02-26 02:53:09 +0300

    The first module is completed

---
+++ script.js
@@ -0,0 +1,68 @@
+// task 1
+// console.log(Math.pow(2, 10));
+
+// task 2
+// console.log(Math.sqrt(245));
+
+// task 3
+// let arr = [4, 2, 5, 19, 13, 0, 10];
+// let sum = 0;
+//
+// for (let elem of arr) {
+//     let cube = elem ** 3;
+//     sum += cube;
+// }
+//
+// console.log(Math.sqrt(sum));
+
+// task 4
+
+// console.log(Math.sqrt(379).toFixed());
+
+// console.log(Math.sqrt(379).toFixed(1));
+
+// console.log(Math.sqrt(379).toFixed(2));
+
+// task 5
+
+// let res = Math.sqrt(587);
+// let obj = {};
+//
+// obj.floor = Math.floor(res);
+// obj.ceil = Math.ceil(res);
+//
+// console.log(obj);
+
+// task 6
+
+// let min = Math.min(4, -2, 5, 19, -130, 0, 10);
+// let max = Math.max(4, -2, 5, 19, -130, 0, 10);
+// console.log(min);
+// console.log(max);
+
+// task 7
+
+// function getRandom(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+//
+// console.log(getRandom(1, 100));
+
+// task 8
+
+// let arr = [];
+//
+// for (let i = 0; i < 10; i++) {
+//     arr[i] = Math.round(Math.random() * 100);
+// }
+//
+// console.log(arr);
+
+// task 9
+
+// function getModule(a, b) {
+//     let dif = a - b;
+//     return Math.abs(dif);
+// }
+//
+// console.log(getModule(9, 18));
